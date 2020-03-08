9ad36973e7f60a309f37eacb086b2b8d454c4e8b
Jared Cunha <jaredcunha@gmail.com>
2020-03-08 16:54:30 -0400

    trap focus

--- src/assets/javascripts/scripts/mobile-nav.js
+++ src/assets/javascripts/scripts/mobile-nav.js
@@ -1,1 +1,25 @@
-console.log("mobile nav js update");
+const OPEN_NAV_BUTTON = document.getElementById('open-nav-menu'),
+      CLOSE_NAV_BUTTON = document.getElementById('close-nav-menu'),
+      NAV_MENU = document.getElementById('nav-flyout'),
+      OPEN_CLASS = "nav__flyout--is-open",
+      VISIBLE_CLASS= "nav__flyout--is-visible";
+
+
+CLOSE_NAV_BUTTON.addEventListener("click", function(e) {
+  //document.body.classList.remove(nav_open_class);
+  NAV_MENU.classList.remove(OPEN_CLASS);
+  OPEN_NAV_BUTTON.focus();
+});
+
+OPEN_NAV_BUTTON.addEventListener("click", function(e) {
+  //document.body.classList.add(nav_open_class);
+  NAV_MENU.classList.add(OPEN_CLASS);
+  CLOSE_NAV_BUTTON.focus();
+
+  setTimeout(function(){
+    //overlay.classList.add(visible_class);
+  }, 100);
+
+  trapFocus(NAV_MENU);
+
+});
