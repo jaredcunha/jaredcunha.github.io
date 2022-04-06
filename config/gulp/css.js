9878eacc9710ce10f3426bf5fbeb13220c7d2ca1
Jared Cunha <jared@coforma.io>
2022-04-06 09:10:49 -0400

    package cleanup

--- config/gulp/css.js
+++ config/gulp/css.js
@@ -1,17 +1,14 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const sassGlob = require('gulp-sass-glob');
-const Fiber = require('fibers');
-
-sass.compiler = require('dart-sass');
 
 gulp.task('sass', function () {
  console.log("\nBuilding CSS");
  return gulp.src('./src/assets/stylesheets/**/*.scss')
    .pipe(sourcemaps.init())
     .pipe(sassGlob())
-    .pipe(sass.sync({outputStyle: 'compressed', fiber: Fiber}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write('maps'))
    .pipe(gulp.dest('./src/css'));
 });
