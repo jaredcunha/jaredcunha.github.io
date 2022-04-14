e0b73973e2f310e7d2418c879574622c9b965215
Jared Cunha <jared@coforma.io>
2022-04-14 09:50:47 -0400

    temp gulp fix

--- config/gulp/images.js
+++ config/gulp/images.js
@@ -1,23 +1,22 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-const flatten = require('gulp-flatten');
-const task = 'images';
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const flatten = require("gulp-flatten");
+const task = "images";
 
-gulp.task('rasters', function (done) {
+gulp.task("rasters", function (done) {
   console.log("\nOptimizing images and moving into './images'");
-  const stream = gulp.src(['./src/assets/images/**/*.*', '!./src/assets/images/_svg/**/*.*'])
-    .pipe(flatten())
-    .pipe(imagemin())
-    .pipe(gulp.dest('./src/images/'));
+  const stream = gulp
+    .src(["./src/assets/images/**/*.*", "!./src/assets/images/_svg/**/*.*"])
+    .pipe(gulp.dest("./src/images/"));
   return stream;
 });
 
-gulp.task('vectors', function (done) {
+gulp.task("vectors", function (done) {
   console.log("\nMove SVGs into './images'");
-  const stream = gulp.src('./src/assets/images/_svg/**/*.*')
-    .pipe(gulp.dest('./src/images/'));
+  const stream = gulp
+    .src("./src/assets/images/_svg/**/*.*")
+    .pipe(gulp.dest("./src/images/"));
   return stream;
 });
 
-
-gulp.task(task, gulp.parallel('vectors'));
+gulp.task(task, gulp.parallel("vectors, rasters"));
