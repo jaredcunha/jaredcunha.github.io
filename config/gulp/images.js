389f1a9a0bd0f1d391480694fadedbb98519931e
Jared Cunha <jared@coforma.io>
2022-04-14 09:55:28 -0400

    other fix attempts

--- config/gulp/images.js
+++ config/gulp/images.js
@@ -3,14 +3,6 @@ const imagemin = require("gulp-imagemin");
 const flatten = require("gulp-flatten");
 const task = "images";
 
-gulp.task("rasters", function (done) {
-  console.log("\nOptimizing images and moving into './images'");
-  const stream = gulp
-    .src(["./src/assets/images/**/*.*", "!./src/assets/images/_svg/**/*.*"])
-    .pipe(gulp.dest("./src/images/"));
-  return stream;
-});
-
 gulp.task("vectors", function (done) {
   console.log("\nMove SVGs into './images'");
   const stream = gulp
@@ -19,4 +11,4 @@ gulp.task("vectors", function (done) {
   return stream;
 });
 
-gulp.task(task, gulp.parallel("vectors, rasters"));
+gulp.task(task, gulp.parallel("vectors"));
