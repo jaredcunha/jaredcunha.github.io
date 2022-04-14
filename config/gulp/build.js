c31443af1c35e2095df9fc43472fcc893198bcbe
Jared Cunha <jared@coforma.io>
2022-04-14 09:45:20 -0400

    clean up gulp

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -1,20 +1,16 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 //var clean = require('gulp-clean');
 
-gulp.task('build',
+gulp.task(
+  "build",
   //gulp.series(
-    gulp.series(
-      'fonts',
-      'images',
-      'scripts',
-      'sass'
-    )
-//  )
+  gulp.series("fonts", "images", "scripts", "sass")
+  //  )
 );
 
-gulp.task('watch', function() {
-  console.log('Running gulp watch');
-  gulp.watch('./src/assets/stylesheets/**/*.scss', gulp.series('sass'));
-  gulp.watch('./src/assets/javascripts/**/*.js', gulp.series('scripts'));
+gulp.task("watch", function () {
+  console.log("Running gulp watch");
+  gulp.watch("./src/assets/stylesheets/**/*.scss", gulp.series("sass"));
+  gulp.watch("./src/assets/javascripts/**/*.js", gulp.series("scripts"));
   //gulp.watch('app/img/*', gulp.series('images'));
 });
