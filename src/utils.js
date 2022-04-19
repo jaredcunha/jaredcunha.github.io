30c4097ffb7abadb4d80f40abcdbbd944e741a3c
Jared Cunha <jared@coforma.io>
2022-04-19 11:35:49 -0400

    remove unsused stuff

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const formatDate = (date) => {
-  const d = new Date(date);
+  const d = new Date(`${date}T06:00-06:00`);
   const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
   const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
   const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
