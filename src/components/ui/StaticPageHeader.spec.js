62ef0bb4bf05fee137c4d5f390d2b1002e9c1595
Jared Cunha <jared@coforma.io>
2022-04-20 14:44:21 -0400

    a11y unit tests

--- src/components/ui/StaticPageHeader.spec.js
+++ src/components/ui/StaticPageHeader.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import StaticPageHeader from './StaticPageHeader';
 import { screen, render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+expect.extend(toHaveNoViolations);
 
 describe('StaticPageHeader', () => {
-  it('renders correctly', () => {
-    render(<StaticPageHeader title="Default Starter" />);
+  it('renders correctly with no accessibility violations', async () => {
+    const { container } = render(<StaticPageHeader title="Default Starter" />);
+    const results = await axe(container);
     expect(screen.getByText('Default Starter')).toBeInTheDocument();
+    expect(results).toHaveNoViolations();
   });
 });
