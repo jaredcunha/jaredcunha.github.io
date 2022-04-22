6fae6e821362a87a26489f8a593845c625fb0f69
Jared Cunha <jared@coforma.io>
2022-04-22 17:45:37 -0400

    enhance mobile nav functionality and add tests

--- src/components/ui/SiteHeader.js
+++ src/components/ui/SiteHeader.js
@@ -1,33 +1,54 @@
 import * as React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 import classNames from 'classnames';
 import { Link } from 'gatsby';
 import SiteLogo from '../../svg/logo.svg';
 import Icon from '../ui/Icon';
 import FocusTrap from 'focus-trap-react';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 const navOpenClass = 'nav-open';
 
 const SiteHeader = () => {
   const openMenuButtonRef = useRef();
   const [menuVisible, setMenuVisible] = useState(false);
+  const { width } = useWindowDimensions();
+  const isWideScreen = width > 720;
 
   const navClassNames = classNames(
     'nav__flyout',
     menuVisible ? 'nav__flyout--is-open' : ''
   );
 
-  const handleMenuToggle = () => {
-    setMenuVisible((menuVisible) => !menuVisible);
+  const openNavMenu = () => {
+    setMenuVisible(true);
   };
 
+  const closeNavMenu = () => {
+    setMenuVisible(false);
+    setTimeout(() => {
+      openMenuButtonRef.current.focus();
+    }, 100);
+  };
+
+  const escFunction = useCallback((event) => {
+    if (event.key === 'Escape') {
+      closeNavMenu();
+    }
+  }, []);
+
   useEffect(() => {
+    // Prevents focus trapping when user expands window while mobile nav is open
+    if (isWideScreen) setMenuVisible(false);
+
     if (menuVisible) {
       document.body.classList.add(navOpenClass);
+      document.addEventListener('keydown', escFunction);
     } else {
       document.body.classList.remove(navOpenClass);
+      document.removeEventListener('keydown', escFunction);
     }
-  }, [menuVisible]);
+  }, [menuVisible, escFunction, isWideScreen]);
 
   return (
     <header>
@@ -35,29 +56,39 @@ const SiteHeader = () => {
         <Link to="/" className="nav__home-link" aria-label="Homepage">
           <SiteLogo />
         </Link>
-        <button
-          type="button"
-          ref={openMenuButtonRef}
-          className="nav__btn nav__btn--open"
-          aria-label="open navigation menu"
-          hidden={menuVisible}
-          onClick={handleMenuToggle}
+        {!menuVisible && (
+          <button
+            type="button"
+            ref={openMenuButtonRef}
+            className="nav__btn nav__btn--open"
+            aria-label="open navigation menu"
+            aria-controls="nav-flyout"
+            onClick={openNavMenu}
+            data-testid="open-nav-button"
+          >
+            <Icon icon="bars" />
+          </button>
+        )}
+        <FocusTrap
+          active={menuVisible}
+          focusTrapOptions={{
+            fallbackFocus: '#nav-flyout',
+            initialFocus: '.nav__btn--close',
+          }}
         >
-          <Icon icon="bars" />
-        </button>
-        <FocusTrap active={menuVisible}>
-          <div className={navClassNames} id="nav-flyout">
+          <div
+            className={navClassNames}
+            id="nav-flyout"
+            data-testid="nav-flyout"
+            tabIndex="-1"
+          >
             <div className="nav_flyout-actions">
               <button
                 type="button"
                 className="nav__btn nav__btn--close"
                 aria-label="Close navigation menu"
-                onClick={() => {
-                  handleMenuToggle();
-                  setTimeout(() => {
-                    openMenuButtonRef.current.focus();
-                  }, 100);
-                }}
+                onClick={closeNavMenu}
+                data-testid="close-nav-button"
               >
                 <Icon icon="times" />
               </button>
--- src/components/ui/SiteHeader.spec.js
+++ src/components/ui/SiteHeader.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SiteHeader from './SiteHeader';
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
 expect.extend(toHaveNoViolations);
@@ -11,5 +11,34 @@ describe('SiteHeader', () => {
     const results = await axe(container);
     expect(screen.getByRole('navigation')).toBeInTheDocument();
     expect(results).toHaveNoViolations();
+  });
+  it('expands to flyout menu on small screens when user clicks menu button', async () => {
+    window.innerWidth = 500;
+    render(<SiteHeader />);
+    const menuButton = screen.getByTestId('open-nav-button');
+    const navMenu = screen.getByTestId('nav-flyout');
+    fireEvent.click(menuButton);
+    await waitFor(() =>
+      expect(navMenu).toHaveClass('nav__flyout nav__flyout--is-open')
+    );
+  });
+  it('sends focus to the close button when mobile nav is opened', async () => {
+    window.innerWidth = 500;
+    render(<SiteHeader />);
+    const menuButton = screen.getByTestId('open-nav-button');
+    const closeButton = screen.getByTestId('close-nav-button');
+    fireEvent.click(menuButton);
+    await waitFor(() => expect(closeButton).toHaveFocus());
+  });
+  it('sends focus to the menu button when the mobile nav is closed', async () => {
+    window.innerWidth = 500;
+    render(<SiteHeader />);
+    const menuButton = screen.getByTestId('open-nav-button');
+    const closeButton = screen.getByTestId('close-nav-button');
+    fireEvent.click(menuButton);
+    fireEvent.click(closeButton);
+    // Have to reset this because of rerender?
+    const menuButton2 = screen.getByTestId('open-nav-button');
+    await waitFor(() => expect(menuButton2).toHaveFocus());
   });
 });
