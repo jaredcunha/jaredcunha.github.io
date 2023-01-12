5c8ca980620b4c740ff755949a528f75167e717d
Jared Cunha <jared@coforma.io>
2023-01-11 21:09:59 -0500

    new blog

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ const IndexPage = () => {
         <div className="hero site-wrap site-wrap--contain">
           <div className="hero__feature">
             <StaticImage
-              src="https://images.jaredcunha.com/_ui/me.png"
+              src="https://ik.imagekit.io/jaredcunhadotcom/_ui/me.png"
               alt=""
               className="hero__img"
               placeholder="none"
